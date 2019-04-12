b1531b9dae8a1591fe0e14f02478539729edb22a
Madhusudan chowdary <k.madhusudan12@gmail.com>
2019-04-12 19:07:43 +0530

    first commit

---
+++ application.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template,request,redirect,url_for
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('game.html')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
